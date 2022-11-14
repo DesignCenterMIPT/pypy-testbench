4e3dc4a7ac030c5ab9cc84e55e5eaa303009635c
Extra5enS <molotkov.md@phystech.edu>
2022-11-14 22:57:48 +0300

    aaaaaaaaaaaaaaaa

--- c-fun/fun.h
+++ c-fun/fun.h
@@ -2,6 +2,8 @@
 #define C_FUN_DEF
 
 #include<stdio.h>
+#include <setjmp.h>
+#include <stdlib.h>
 
 void hello(void);
 
@@ -10,5 +12,18 @@ void printInt(int);
 int returnInt(int);
 
 int sumInt(int, int);
+
+void exception_init();
+
+void exception_new_point();
+void exception_delete();
+
+jmp_buf* exception_top();
+
+void exception_free();
+
+int try_catch();
+void try_catch_end();
+void throw();
 
 #endif
